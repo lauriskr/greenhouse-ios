5b29b438c042ca44b32e04766afe122555e50332
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2014-10-07 18:18:08 +0300

    Make tests faster

--- greenhouse-ios/greenhouse-iosTests/greenhouse_iosTests.m
+++ greenhouse-ios/greenhouse-iosTests/greenhouse_iosTests.m
@@ -18,19 +18,17 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    [NSThread sleepForTimeInterval:1.0f];
 }
 
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-    [NSThread sleepForTimeInterval:1.0f];
 }
 
 - (void)testFail
 {
-    for (int i = 1; i <= 9999; i++)
+    for (int i = 1; i <= 4000; i++)
     {
         NSLog(@"%d", i);
     }
@@ -39,19 +37,19 @@
 
 - (void)testFalse
 {
-    for (int i = 1; i <= 9999; i++)
-    {
-        NSLog(@"%d", i);
-    }
+//    for (int i = 1; i <= 9999; i++)
+//    {
+//        NSLog(@"%d", i);
+//    }
     XCTAssertFalse(NO);
 }
 
 - (void)testTrue
 {
-    for (int i = 1; i <= 9999; i++)
-    {
-        NSLog(@"%d", i);
-    }
+//    for (int i = 1; i <= 9999; i++)
+//    {
+//        NSLog(@"%d", i);
+//    }
     XCTAssertTrue(YES);
 }
 
