9fb80004dca95d85f7da163d09615e6a977c1937
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2014-11-05 12:22:10 +0200

    Add static lib target

---
+++ greenhouse-ios/staticlib/staticlib.h
@@ -0,0 +1,13 @@
+//
+//  staticlib.h
+//  staticlib
+//
+//  Created by lauris on 05/11/14.
+//  Copyright (c) 2014 greenhouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface staticlib : NSObject
+
+@end
---
+++ greenhouse-ios/staticlib/staticlib.m
@@ -0,0 +1,13 @@
+//
+//  staticlib.m
+//  staticlib
+//
+//  Created by lauris on 05/11/14.
+//  Copyright (c) 2014 greenhouse. All rights reserved.
+//
+
+#import "staticlib.h"
+
+@implementation staticlib
+
+@end
