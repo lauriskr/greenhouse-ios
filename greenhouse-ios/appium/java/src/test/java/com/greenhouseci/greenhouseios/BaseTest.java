382d964d03ef24f4a4d4b16f18189bdccb0bc895
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2015-01-22 16:18:16 +0200

    Improve java tests, add php tests as well

---
+++ greenhouse-ios/appium/java/src/test/java/com/greenhouseci/greenhouseios/BaseTest.java
@@ -0,0 +1,40 @@
+package com.greenhouseci.greenhouseios;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.URL;
+
+public class BaseTest {
+
+    private AppiumDriver driver;
+
+    private WebElement row;
+
+    @Before
+    public void setUp() throws Exception {
+        // set up appium
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "../../../../greenhouse-ios/greenhouse-ios/build/Debug-iphonesimulator");
+        File app = new File(appDir, "greenhouse-ios.app");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+        capabilities.setCapability("platformVersion", "8.1");
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("deviceName", "iPhone Simulator");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+    
+}
---
+++ greenhouse-ios/appium/php/SimpleTest.php
@@ -0,0 +1,42 @@
+<?php
+// To run this test, install Sausage (see http://github.com/jlipps/sausage-bun
+// to get the curl one-liner to run in this directory), then run:
+//     vendor/bin/phpunit SimpleTest.php
+
+require_once "vendor/autoload.php";
+define("APP_PATH", realpath(dirname(__FILE__).'/../../build/Debug-iphonesimulator/greenhouse-ios.app'));
+if (!APP_PATH) {
+    die("App did not exist!");
+}
+
+
+class SimpleTest extends Sauce\Sausage\WebDriverTestCase
+{
+    protected $numValues = array();
+
+    public static $browsers = array(
+        array(
+            'local' => true,
+            'port' => 4723,
+            'browserName' => '',
+            'desiredCapabilities' => array(
+                'device' => 'iPhone Simulator',
+				'deviceName' => 'iPhone Simulator',
+                'version' => '8.1',
+                'platform' => 'iOS',
+				'platformName' => 'iOS',
+                'app' => APP_PATH
+            )
+        )
+    );
+
+    public function testExampleFail()
+    {
+        $this->assertTrue(false);
+    }
+
+	public function testExampleSuccess()
+    {
+        $this->assertFalse(false);
+    }
+}
