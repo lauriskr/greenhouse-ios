db44f86740c9fb67022a91df7a52e871e8aa6f9f
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2015-01-22 14:35:26 +0200

    Add java appium tests

---
+++ greenhouse-ios/appium/src/test/java/com/saucelabs/appium/UICatalogTest.java
@@ -0,0 +1,77 @@
+package com.saucelabs.appium;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * <a href="https://github.com/appium/appium">Appium</a> test which runs against a local Appium instance deployed
+  * with the 'UICatalog' iPhone project which is included in the Appium source distribution.
+ *
+ * @author Ross Rowe
+ */
+public class UICatalogTest {
+
+    private AppiumDriver driver;
+
+    private WebElement row;
+
+    @Before
+    public void setUp() throws Exception {
+        // set up appium
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "../../../greenhouse-ios/greenhouse-ios/build/Debug-iphonesimulator");
+        File app = new File(appDir, "greenhouse-ios.app");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+        capabilities.setCapability("platformVersion", "8.1");
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("deviceName", "iPhone Simulator");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void testExample() {
+        assertTrue(false);
+    }
+}
