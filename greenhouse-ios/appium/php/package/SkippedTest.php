e7d17e1ea874b08c3fa8613c8e312d6106267d53
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2015-02-10 15:41:04 +0200

    Add test file that is skipped

---
+++ greenhouse-ios/appium/php/package/SkippedTest.php
@@ -0,0 +1,20 @@
+<?php
+// To run this test, install Sausage (see http://github.com/jlipps/sausage-bun
+// to get the curl one-liner to run in this directory), then run:
+//     vendor/bin/phpunit SimpleTest.php
+
+require_once "BaseTest.php";
+
+class SkippedTest extends BaseTest
+{
+
+	public function testExampleFail()
+	{
+		$this->assertTrue(false);
+	}
+
+	public function testExampleSuccess()
+	{
+		$this->assertFalse(false);
+	}
+}
